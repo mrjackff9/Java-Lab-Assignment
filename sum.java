c3181d4de5c9ba754ed48a8c422f16fe1002a5ed
mrjackff9 <143486756+mrjackff9@users.noreply.github.com>
2023-08-29 13:58:10 +0530

    Java Lab

---
+++ sum.java
@@ -0,0 +1,14 @@
+class sum{
+  public static void main(String[]args){
+    int num1=10;
+    int num2=15;
+    int addition=num1+num2;
+    int subtract=num1-num2;
+    int divide=num1/num2;
+    int multiply=num1*num2;
+    System.out.println("Addition is "+num1+"+"+num2+"="+addition);
+    System.out.println("Subtraction is "+num1+"-"+num2+"="+subtract);
+    System.out.println("Divition is "+num1+"/"+num2+"="+divide);
+    System.out.println("Multilipation is "+num1+"x"+num2+"="+multiply);
+  }
+}
---
+++ xyz.java
@@ -0,0 +1,11 @@
+class xyz{
+void print()
+{
+System.out.println("Hello World");
+}
+public static void main(String[]args)
+{
+xyz m=new xyz();
+m.print();
+}
+}
