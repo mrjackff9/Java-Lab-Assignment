9ec0b0db0030adf46de13e2372a63dc54eeeda3f
mrjackff9 <143486756+mrjackff9@users.noreply.github.com>
2023-08-29 14:42:26 +0530

    Create average.java

---
+++ average.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class avgerage{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enetr three Numebr :");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int d=(a+b+c)/3;
+        System.out.println("Average is "+d);
+    }
+}
