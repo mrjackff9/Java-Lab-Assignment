d62d407146bcfbc2843cfd07ab7285c4b3c6c79f
mrjackff9 <143486756+mrjackff9@users.noreply.github.com>
2023-09-05 12:45:29 +0530

    Add files via upload

---
+++ averageofarray.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class averageofarray{
+public static void main(String[]args){
+	int []arr=new int[]{10,20,30,40,50};
+	int sum=0;
+	int avg=0;
+	for (int i=0;i<arr.length;i++){
+		sum=sum+arr[i];
+		avg=sum/arr.length;
+		}
+	System.out.println("Average of Array is "+avg);
+	}
+}
